e7d779fed00291b69390642ec6f51788259d0469
Utkarsh <id.simpu@gmail.com>
2020-10-04 01:17:25 +0530

    Finished the parser

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,57 @@
-use std::env;
 use std::fs::File;
 use std::io::BufReader;
-use xml::EventReader;
+use xml::{EventReader, ParserConfig};
 use xml::reader::XmlEvent;
 use corpus::Corpus;
+use clap::Clap;
 
 mod corpus;
 
-fn get_file(args: Vec<String>) -> Option<File> {
-    match args.get(1) {
-        Some(path) => {
-            match File::open(path) {
-                Ok(file) => { return Some(file); }
-                Err(error) => { println!("filed reading failed. {}", error); }
-            }
+#[derive(Clap)]
+#[derive(Debug)]
+#[clap(version = "1.0", author = "Simpu <id.simpu@gmail.com>")]
+struct Opts {
+    read: String,
+    write: String,
+    #[clap(short, long, parse(from_occurrences))]
+    lowercase: i32,
+}
+
+fn read_file(path: &str) -> Option<File> {
+    match File::open(path) {
+        Ok(file) => Some(file),
+        Err(error) => {
+            println!("File: {} Error: {}", path, error);
+            None
         }
-        None => println!("argument parsing failed"),
-    };
-    None
+    }
+}
+
+fn write_file(path: &str) -> Option<File> {
+    match File::create(path) {
+        Ok(file) => Some(file),
+        Err(error) => {
+            println!("File: {} Error: {}", path, error);
+            None
+        }
+    }
+}
+
+fn to_bool(value: i32) -> bool {
+    if value == 0 {
+        false
+    } else {
+        true
+    }
 }
 
 fn main() {
-    if let Some(file) = get_file(env::args().collect()) {
-        let reader = EventReader::new(BufReader::new(file));
-        let mut corpus = Corpus::new();
+    let opts: Opts = Opts::parse();
+    if let (Some(read), Some(write)) = (read_file(opts.read.as_str()), write_file(opts.write.as_str())) {
+        let buff = BufReader::new(read);
+        let config = ParserConfig::default().trim_whitespace(true);
+        let reader = EventReader::new_with_config(buff, config);
+        let mut corpus = Corpus::new(write, to_bool(opts.lowercase));
         for event in reader {
             match event {
                 Ok(event) => {
